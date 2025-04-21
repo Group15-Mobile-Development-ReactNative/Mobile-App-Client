a82aa8ce8513ae5e6b00baeab0d268571ff0861c
Ryan_DIN23SP <ryandilthusha@gmail.com>
2025-04-22 01:25:40 +0300

    Screenshots uupdated

--- constants/agoraConfig.ts
+++ constants/agoraConfig.ts
@@ -1,4 +1,4 @@
-export const AGORA_APP_ID = "c305b6bcde654e3f8c38fd91485fda30";
-export const AGORA_TEMP_TOKEN = "007eJxTYAiasN3+NPcpqe3SvbmBS6cuyvpTpxPdUeJdfWK/p2qRUq8CQ7KxgWmSWVJySqqZqUmqcZpFsrFFWoqloYmFaVpKorGB7yK2jIZARoYD6sIMjFAI4nMzOGckljgWFDgn5uQwMAAA5T8gXQ=="; // For testing only
+export const AGORA_APP_ID = "8778573e33d44f059cbfe7f58119b993";
+export const AGORA_TEMP_TOKEN = "007eJxTYKibstW5qvw3+44pXhGbOzQ/H6rka78qOo/peFWKr8/jni0KDBbm5ham5sapxsYpJiZpBqaWyUlpqeZpphaGhpZJlpbGZ46wZTQEMjLYrvvCysgAgSA+N4NzRmKJY0GBc2JODgMDAEuHIkQ="; // For testing only
 export const CHANNEL_NAME = "ChatAppCall".trim();; 
 // Link: https://console.agora.io/project/RdM9-OFai
